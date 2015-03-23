831f097da7a3d39d1dfdfcf7881aeb576da6a629
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2015-03-23 11:46:31 -0300

    Project Module 4 snapshot
    Module 4 - configuration using annotations, finished.

--- src/com/pluralsight/service/CustomerServiceImpl.java
+++ src/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,29 +2,31 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 	
-	public CustomerServiceImpl() {}
-
-	public void setCustomerRepository(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
-
+	@Autowired
 	private CustomerRepository customerRepository;
 	
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
-	@Override
-	public List<Customer> findAll() {
-		return customerRepository.findAll();
-	}
+	public CustomerServiceImpl() {}
 	
 	public CustomerServiceImpl(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}	
+		
+	@Override
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
 	}
 	
 }
