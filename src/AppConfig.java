c341cc76138ac3c2b63bdb1dbf2829ce193b4001
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2015-03-25 10:49:46 -0300

    Project Module 6 snapshot

--- src/AppConfig.java
+++ src/AppConfig.java
@@ -1,6 +1,7 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
@@ -10,11 +11,12 @@ import com.pluralsight.service.CustomerServiceImpl;
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
-	@Bean(name="customerService")
+	@Bean(name="customerService")	
+	@Scope("prototype")
 	public CustomerService getCustomerService() {
-		CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+		CustomerServiceImpl customerService = new CustomerServiceImpl();
 		//customerServiceImpl.setCustomerRepository(getCustomerRepository());
-		return customerServiceImpl; 		
+		return customerService; 		
 	}
 	
 //	@Bean(name ="customerRepository")
--- src/com/pluralsight/service/CustomerServiceImpl.java
+++ src/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,15 +3,13 @@ package com.pluralsight.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
-@Service("customerService")
+
 public class CustomerServiceImpl implements CustomerService {
 	
-	@Autowired
 	private CustomerRepository customerRepository;
 	
 	public CustomerServiceImpl() {}
@@ -21,7 +19,9 @@ public class CustomerServiceImpl implements CustomerService {
 		this.customerRepository = customerRepository;
 	}
 
+	@Autowired
 	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("inside setter");
 		this.customerRepository = customerRepository;
 	}	
 		
